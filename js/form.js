7015f96ee21df3a395cb5b1ea0c82fc13adfad2d
Elly Loel <hello@ellyloel.com>
2024-11-24 12:29:45 +1100

    Removing unneeded motion preference checks, this is already handled in the styles

--- js/form.js
+++ js/form.js
@@ -3,22 +3,15 @@ const sort = document.getElementById("sort");
 const productList = document.querySelector(".most-popular-container");
 const filterTargets = document.querySelectorAll(`section .product-card`);
 
-// get the users motion preference
-const { matches: motionOK } = window.matchMedia("(prefers-reduced-motion: no-preference)");
-
 // when the user changes the form radio group selected value
 filter.addEventListener("input", async (event) => {
 	// extract their choice
 	const category = event.target.value;
 
 	// this gives each product a unique view transition name
-	// only if the user is ok with motion, because this will enable position morphing
-	// by default they crossfade which is great for "no motion"
-	if (motionOK) {
-		filterTargets.forEach((item, index) => {
-			item.parentNode.style.viewTransitionName = `product-${index}`;
-		});
-	}
+	filterTargets.forEach((item, index) => {
+		item.parentNode.style.viewTransitionName = `product-${index}`;
+	});
 
 	// the filter mutation: just display: none if no match
 	function filter() {
@@ -33,11 +26,9 @@ filter.addEventListener("input", async (event) => {
 	document.startViewTransition ? (vt = document.startViewTransition(() => filter())) : filter();
 	if (!vt) return;
 	await vt.finished;
-	if (motionOK) {
-		filterTargets.forEach((item, index) => {
-			item.parentNode.style.viewTransitionName = "";
-		});
-	}
+	filterTargets.forEach((item, index) => {
+		item.parentNode.style.viewTransitionName = "";
+	});
 });
 
 sort.addEventListener("input", async (event) => {
@@ -47,13 +38,9 @@ sort.addEventListener("input", async (event) => {
 	const productCardContainers = Array.from(productList.querySelectorAll(".product-card-container"));
 
 	// this gives each product a unique view transition name
-	// only if the user is ok with motion, because this will enable position morphing
-	// by default they crossfade which is great for "no motion"
-	if (motionOK) {
-		productCardContainers.forEach((productCardContainer, index) => {
-			productCardContainer.style.viewTransitionName = `product-${index}`;
-		});
-	}
+	productCardContainers.forEach((productCardContainer, index) => {
+		productCardContainer.style.viewTransitionName = `product-${index}`;
+	});
 
 	// Function to sort products
 	function sort() {
@@ -87,9 +74,7 @@ sort.addEventListener("input", async (event) => {
 	document.startViewTransition ? (vt = document.startViewTransition(() => sort())) : sort();
 	if (!vt) return;
 	await vt.finished;
-	if (motionOK) {
-		productCardContainers.forEach((productCardContainer, index) => {
-			productCardContainer.parentNode.style.viewTransitionName = "";
-		});
-	}
+	productCardContainers.forEach((productCardContainer, index) => {
+		productCardContainer.parentNode.style.viewTransitionName = "";
+	});
 });
