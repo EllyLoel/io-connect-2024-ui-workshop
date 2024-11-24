cfc52ce3e76d1f41b95e3926359373a802dc270f
Elly Loel <hello@ellyloel.com>
2024-11-24 12:29:59 +1100

    Fixing the add to cart view transition

--- js/cart.js
+++ js/cart.js
@@ -4,26 +4,23 @@ const cartButton = document.querySelector('button[aria-label="Cart"]');
 // Get all add to cart buttons
 const addToCartButtons = document.querySelectorAll('button[id^="add-to-cart-"]');
 
-let cartItemCount = 0; // Initialize cart item count
+let cartItemCount = 0;
 
 addToCartButtons.forEach(async (button) => {
 	button.addEventListener("click", async (event) => {
-		cartItemCount++; // Increment cart item count
-
-		// Update cart count
-		cartButton.dataset.count = cartItemCount;
+		cartButton.dataset.count = ++cartItemCount;
 
 		const productCardContainer = button.parentElement.parentElement.parentElement;
-		const viewTransitionName = getComputedStyle(productCardContainer).getPropertyValue("view-transition-name");
+		const viewTransitionName = "product";
+		productCardContainer.style.viewTransitionName = viewTransitionName;
 
 		const viewTransition = document?.startViewTransition(() => {
-			productCardContainer.style.viewTransitionName = "none";
+			productCardContainer.style.viewTransitionName = "";
 			cartButton.lastElementChild.style.viewTransitionName = viewTransitionName;
 		});
 
 		await viewTransition.finished;
 
-		productCardContainer.style.viewTransitionName = viewTransitionName;
-		cartButton.lastElementChild.style.viewTransitionName = "none";
+		cartButton.lastElementChild.style.viewTransitionName = "";
 	});
 });
